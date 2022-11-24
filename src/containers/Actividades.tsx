bb811f4de745430435c58710846dac953ef9ab5d
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2022-11-24 14:29:51 -0500

    Ejemplo card

--- src/containers/Actividades.tsx
+++ src/containers/Actividades.tsx
@@ -1,6 +1,9 @@
-import { ExpandMore } from "@mui/icons-material";
+import { ExpandMore, Today } from '@mui/icons-material';
 import {
   Box,
+  Card,
+  CardContent,
+  CardHeader,
   Chip,
   FormControl,
   IconButton,
@@ -9,17 +12,17 @@ import {
   Select,
   Stack,
   Typography,
-} from "@mui/material";
+} from '@mui/material';
 
 export const Actividades = () => {
   return (
-    <Box display="flex" p={2}>
+    <Box display="flex" flexDirection="column">
       <Stack
         direction="row"
         alignItems="center"
         justifyContent="space-between"
         spacing={2}
-        width="100%"
+        p={2}
       >
         <Stack direction="row" spacing={1}>
           <Chip label="Todas" variant="outlined" color="primary" />
@@ -35,7 +38,34 @@ export const Actividades = () => {
           </IconButton>
         </Stack>
       </Stack>
-      <Box></Box>
+      <Box>
+        <Card>
+          <CardHeader
+            title="Acta de entrega Apto. 1705"
+            titleTypographyProps={{ variant: 'body1' }}
+          />
+          <CardContent sx={{ py: 0 }}>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Chip variant="filled" label="Confirmar recibido" />
+              <Stack direction="row" gap={1} alignItems="center">
+                <Today />
+                <Stack alignItems="center">
+                  <Typography variant="caption" component="div">
+                    Vencimiento:
+                  </Typography>
+                  <Typography variant="body2" component="div">
+                    30/10/2022
+                  </Typography>
+                </Stack>
+              </Stack>
+            </Stack>
+          </CardContent>
+        </Card>
+      </Box>
     </Box>
   );
 };
