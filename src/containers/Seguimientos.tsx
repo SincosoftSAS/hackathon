1243cb43c19a738c99ad9a3746455aa70b5438b3
AlbeiroFranco <jose.arleck91@hotmail.com>
2022-11-25 14:01:28 -0600

    buttonNavigationBar

--- src/containers/Seguimientos.tsx
+++ src/containers/Seguimientos.tsx
@@ -1,19 +1,25 @@
+import { AttachFile, Create, History } from "@mui/icons-material";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import DescriptionIcon from '@mui/icons-material/Description';
-import { AppBar, BottomNavigation, BottomNavigationAction, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, BottomNavigation, BottomNavigationAction, Box, IconButton, Toolbar, Typography, Stack, Paper } from '@mui/material';
+import { positions } from "@mui/system";
 import { useState } from "react";
 import { Seguimiento } from "../components/Seguimiento";
 import { Actividades } from "./Actividades";
+import { light } from '../theme/variables';
+import React from "react";
 
 export const Seguimientos = () => {
   const [tab, setTab] = useState(0);
 
   const tabs = [<Actividades />, <Actividades />];
 
+  const [value, setValue] = React.useState(0);
+
   return (
     <>
-      <Box width="100%" position="fixed" zIndex={1}>
-        <AppBar position="static" >
+      <Box width="100%">
+        <AppBar position="fixed"  >
           <Toolbar>
             <IconButton color="inherit">
               <ChevronLeftIcon />
@@ -33,9 +39,14 @@ export const Seguimientos = () => {
         <Seguimiento></Seguimiento>
         <Seguimiento></Seguimiento>
         <Seguimiento></Seguimiento>
-        <BottomNavigation>
-          <BottomNavigationAction label="Recents" icon={<DescriptionIcon />} />
+        <Paper sx={{position: 'fixed', bottom : 0, left: 0, right: 0}} elevation={3}>
+        <BottomNavigation value={value} showLabels sx={{paddingX:3}} >
+          <BottomNavigationAction label="Resumen" icon={<DescriptionIcon />} />
+          <BottomNavigationAction label="Adjuntos" icon={<AttachFile />} />
+          <BottomNavigationAction label="Editar" icon={<Create />} />
+          <BottomNavigationAction label="Seguimiento" icon={<History />}  />
         </BottomNavigation>
+        </Paper>
       </Box>
       {/* bottom bar mnu */}
 
